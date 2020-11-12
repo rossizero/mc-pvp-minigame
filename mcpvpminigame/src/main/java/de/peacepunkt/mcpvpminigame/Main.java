bac3930498e422c21ad695e7fade57b01d2addba
Rossi <sebastian.rossi@elkiro.de>
2020-11-12 14:02:39 +0100

    Initial plugin setup

---
+++ mcpvpminigame/src/main/java/de/peacepunkt/mcpvpminigame/Main.java
@@ -0,0 +1,15 @@
+package de.peacepunkt.mcpvpminigame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+        @Override
+        public void onEnable() {
+            System.out.println("I AM HERE!!!");
+        }
+
+        @Override
+        public void onDisable() {
+
+        }
+}
