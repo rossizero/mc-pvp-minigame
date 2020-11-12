31e504c2af0808fb9a51633daec493a67bf0602b
Rossi <sebastian.rossi@elkiro.de>
2020-11-12 16:05:16 +0100

    Make people spawn in lobby world.

--- mcpvpminigame/src/main/java/de/peacepunkt/mcpvpminigame/Main.java
+++ mcpvpminigame/src/main/java/de/peacepunkt/mcpvpminigame/Main.java
@@ -1,15 +1,66 @@
 package de.peacepunkt.mcpvpminigame;
 
+import org.bukkit.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+
+public class Main extends JavaPlugin implements Listener {
+        World lobby;
+
         @Override
         public void onEnable() {
-            System.out.println("I AM HERE!!!");
+            lobby = checkLobby();
+            System.out.println(lobby);
+            getServer().getPluginManager().registerEvents(this, this);
+            new OpCommands(this);
         }
 
         @Override
         public void onDisable() {
 
+        }
+        @EventHandler
+        public void onPlayerJoin(PlayerJoinEvent event) {
+                event.getPlayer().sendMessage("welcome");
+                Location l = new Location(lobby, lobby.getSpawnLocation().getX(), lobby.getSpawnLocation().getY(), lobby.getSpawnLocation().getZ());
+                System.out.println(l);
+                event.getPlayer().teleport(l);
+        }
+        private World checkLobby() {
+                System.out.println("");
+                for(World w : Bukkit.getWorlds()) {
+                        System.out.println(w);
+                }
+                System.out.println("");
+
+                System.out.println("loading lobby");
+                World ret = Bukkit.getWorld("lobby");
+                if(ret == null) {
+                        System.out.println("starting world creation");
+                        WorldCreator creator = new WorldCreator("lobby");
+                        //creator.type(WorldType.CUSTOMIZED);
+                        creator.environment(World.Environment.NORMAL);
+                        creator.generateStructures(false);
+                        creator.generator(new SpawnWorldGenerator());
+                        World w = Bukkit.createWorld(creator);
+                        w.setGameRule(GameRule.DO_FIRE_TICK, false);
+                        w.setGameRule(GameRule.DO_MOB_SPAWNING, false);
+                        w.setGameRule(GameRule.DO_PATROL_SPAWNING, false);
+                        w.setGameRule(GameRule.DO_TRADER_SPAWNING, false);
+                        w.setGameRule(GameRule.MOB_GRIEFING, false);
+                        w.setGameRule(GameRule.RANDOM_TICK_SPEED, 0);
+                        w.setGameRule(GameRule.DO_WEATHER_CYCLE, false);
+                        w.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+                        w.setSpawnLocation(0, 151, 0);
+                        w.setTime(1000);
+                        System.out.println("World creation successful.");
+                        ret = w;
+                        System.out.println(ret);
+
+                }
+                return ret;
         }
 }
---
+++ mcpvpminigame/src/main/java/de/peacepunkt/mcpvpminigame/OpCommands.java
@@ -0,0 +1,40 @@
+package de.peacepunkt.mcpvpminigame;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class OpCommands {
+    public OpCommands(Main main) {
+        main.getCommand("warp").setExecutor(new CommandExecutor() {
+            @Override
+            public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+                if (arg0 instanceof Player) {
+                    if(arg3.length == 1) {
+                        Player p = (Player) arg0;
+                        String worldName = arg3[0];
+                        boolean done = false;
+                        World target = Bukkit.getWorld(worldName);
+                        if(target != null) {
+                            Location loc = new Location(target, target.getSpawnLocation().getX(),target.getSpawnLocation().getY(), target.getSpawnLocation().getZ());
+                            p.teleport(loc);
+                            done = true;
+                        }
+                        if(!done) {
+                            p.sendMessage("World " + arg3[0] + " could be unloaded.");
+                        }
+                        return true;
+                    } else {
+                        Player p = (Player) arg0;
+                        p.sendMessage("/warp <worldName>");
+                    }
+                }
+                return false;
+            }
+        });
+    }
+}
